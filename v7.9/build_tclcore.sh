5aa298e342034f24d2bb0f5ccde4cf1c316a47b9
zeb <zebbey@gmail>
2016-08-08 23:26:45 +0800

    dejaGNU done

---
+++ v7.9/build_tclcore.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+pushd $LFS/sources/
+
+rm -rf tcl8.6.4
+
+tar xvf tcl-core8.6.4-src.tar.gz
+
+cd tcl8.6.4/unix
+
+./configure --prefix=/tools
+
+make
+
+TZ=UTC make test
+
+make install
+
+chmod -v u+w /tools/lib/libtcl8.6.so
+
+make install-private-headers
+
+ln -sv tclsh8.6 /tools/bin/tclsh
+
